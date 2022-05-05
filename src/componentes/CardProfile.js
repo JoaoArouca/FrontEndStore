ee2fe09e1375f4ccbc9b88ac29e8f77fea410e24
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-05-05 12:16:32 -0300

    css header, reorganiza header

---
+++ src/componentes/CardProfile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactComponent as ProfileIcon } from '../icons/ProfileIcon.svg';
+
+function CardProfile() {
+  return (
+    <ProfileIcon />
+  );
+}
+
+export default CardProfile;
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Storecontext from '../context/StoreContext';
 import { getProducts } from '../services/getAPI';
 import CardLocation from './CardLocation';
+import CardProfile from './CardProfile';
 import NavCategories from './NavCategories';
 
 function Header() {
@@ -19,20 +20,24 @@ function Header() {
   return (
     <header className="container header">
       <NavCategories />
-      <form>
-        <input // formulário de pesquisa
-          type='text'
-          placeholder='Buscar produtos, marcas e muito mais...'
-          onChange={ handleInputChange }
-        />
-        <button
-          type='button'
-          onClick={ () => fetch(search) }
-        >
-          Pesquisar
-        </button>
-      </form>
-      <CardLocation />
+      <section className='sectionHeader container'>
+        <form>
+          <input // formulário de pesquisa
+            type='text'
+            placeholder='Buscar produtos, marcas e muito mais...'
+            onChange={ handleInputChange }
+          />
+          <button
+            type='button'
+            onClick={ () => fetch(search) }
+          >
+            Pesquisar
+          </button>
+        </form>
+        <CardLocation />
+        <CardProfile />
+      </section>
+      
     </header>
   );
 }
